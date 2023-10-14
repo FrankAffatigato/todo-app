5fc05f2224e419bfb0dead08d66de57b653d6f96
Frank Affatigato <ifrankdavida@gmail.com>
2023-10-14 11:48:25 -0500

    Finished Edit Button

--- Main/gui.py
+++ Main/gui.py
@@ -2,7 +2,7 @@ import function as fn
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter task")
+input_box = sg.InputText(tooltip="Enter task", key='todo')
 add_button = sg.Button("Add")
 
 list_box = sg.Listbox(values=fn.get_todos("text.txt"), key='todos',
@@ -18,18 +18,25 @@ window = sg.Window('My To-Do App', layout=[[label], [add_button, input_box],
 #Keep application open until it is intentionally closed
 while True:
     event, value = window.read()
-    print(event)
+
     if event == "Add": 
         todos = fn.get_todos("text.txt")
-        todos.append(value[0] + "\n")
+        todos.append(value['todo'] + "\n")
         fn.write_todos("text.txt", todos)
-        print(event, value)
-    
+        window['todos'].update(values=todos)
+
+    elif event == "todos":
+        print(value["todos"])
+        window['todo'].update(value=value['todos'][0])
+        todos = fn.get_todos()
+        todos_index = todos.index(value["todos"][0])
+
     elif event == "Edit":
         todos = fn.get_todos()
-        print(todos)
-        print(event)
-        print(value)
+        todos[todos_index] = value['todo'] + "\n"
+        fn.write_todos("text.txt", todos)
+        window['todos'].update(values=todos)
+
     elif event == sg.WIN_CLOSED:
         break
 
